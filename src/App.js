c5332135aa57749ac911505d85d00cc1b039d7f0
Katia Nienow <nienowkatia@gmail.com>
2023-03-06 19:33:40 -0300

    requisito 3 e correção dos erros do lint

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
 import React from 'react';
-import Form from './components/Form'
+import Form from './components/Form';
+import Card from './components/Card';
 
 class App extends React.Component {
   render() {
     return (
       <div>
         <form>
-          <Form />
+          <Form 
+            cardName={cardName}
+            cardDescription={cardDescription}
+            cardAttr1={cardAttr1}
+            cardAttr2={cardAttr2}
+            cardAttr3={cardAttr3}
+            cardImage={cardImage}
+            cardRare={cardRare}
+            cardTrunfo={cardTrunfo}
+          />
         </form>
+        <Card 
+          cardName={cardName}
+          cardDescription={cardDescription}
+          cardAttr1={cardAttr1}
+          cardAttr2={cardAttr2}
+          cardAttr3={cardAttr3}
+          cardImage={cardImage}
+          cardRare={cardRare}
+          cardTrunfo={cardTrunfo}
+        />
       </div>
     );
   }
