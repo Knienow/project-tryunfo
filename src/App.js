4e3c5f5702e01cbc4a99751c09c8be52c9200c7d
Katia Nienow <nienowkatia@gmail.com>
2023-03-06 19:39:56 -0300

    correção de erros do lint

--- src/App.js
+++ src/App.js
@@ -7,26 +7,26 @@ class App extends React.Component {
     return (
       <div>
         <form>
-          <Form 
-            cardName={cardName}
-            cardDescription={cardDescription}
-            cardAttr1={cardAttr1}
-            cardAttr2={cardAttr2}
-            cardAttr3={cardAttr3}
-            cardImage={cardImage}
-            cardRare={cardRare}
-            cardTrunfo={cardTrunfo}
+          <Form
+            cardName={ cardName }
+            cardDescription={ cardDescription }
+            cardAttr1={ cardAttr1 }
+            cardAttr2={ cardAttr2 }
+            cardAttr3={ cardAttr3 }
+            cardImage={ cardImage }
+            cardRare={ cardRare }
+            cardTrunfo={ cardTrunfo }
           />
         </form>
-        <Card 
-          cardName={cardName}
-          cardDescription={cardDescription}
-          cardAttr1={cardAttr1}
-          cardAttr2={cardAttr2}
-          cardAttr3={cardAttr3}
-          cardImage={cardImage}
-          cardRare={cardRare}
-          cardTrunfo={cardTrunfo}
+        <Card
+          cardName={ cardName }
+          cardDescription={ cardDescription }
+          cardAttr1={ cardAttr1 }
+          cardAttr2={ cardAttr2 }
+          cardAttr3={ cardAttr3 }
+          cardImage={ cardImage }
+          cardRare={ cardRare }
+          cardTrunfo={ cardTrunfo }
         />
       </div>
     );
